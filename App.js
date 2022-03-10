89dc729b6d5bdc9f695ba1ccff7beeb85668c9c1
camjesus <32471798+camjesus@users.noreply.github.com>
2022-03-09 22:47:07 -0300

    change App.js

--- App.js
+++ App.js
@@ -5,11 +5,15 @@ import { useFonts } from 'expo-font';
 import FuturasMaratones from './components/FuturasMaratones';
 import AppLoading from 'expo-app-loading';
 import Typography from './constants/Typography';
+import {setName} from './store/actions/name.action'
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import store from './store'
 
 
 export default function Main() {
-  const [user, setUser] = useState('')
-
+  //const [user, setUser] = useState('')
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.name);
   const [loaded] = useFonts({
     Lato: require('./assets/fonts/ArchitectsDaughter-Regular.ttf'),
     [Typography.titleFont]: require('./assets/fonts/ArchitectsDaughter-Regular.ttf'),
@@ -18,7 +22,8 @@ export default function Main() {
   if (!loaded) return <AppLoading />
 
   const handleSetUser = userSelected =>{
-    setUser(userSelected)
+    //setUser(userSelected)
+    dispatch(setName(userSelected));
   }
   let content =<Login setUserLogin={handleSetUser} />
 
@@ -29,8 +34,10 @@ export default function Main() {
   
 
   return (
-    <PaperProvider>
-      {content}
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider>
+        {content}
+      </PaperProvider>
+    </Provider>
   );
 }
